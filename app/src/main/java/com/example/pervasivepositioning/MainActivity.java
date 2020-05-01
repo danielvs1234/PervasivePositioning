c4ce3874167f89c9cb65540ff920776346f3838e
danielvs1234 <danielvs@live.dk>
2020-05-02 00:01:13 +0200

    First commit
    Done application, one button for scanning the networks
    one button for getting the Dbm connection to the chosen network.

--- app/src/main/java/com/example/pervasivepositioning/MainActivity.java
+++ app/src/main/java/com/example/pervasivepositioning/MainActivity.java
@@ -1,32 +1,72 @@
 package com.example.pervasivepositioning;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     WifiSignalDataCollector wifiSignal;
 
     Button signalButton;
+    Button scanButton;
     TextView dataTextView;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int PERMISSION_REQUEST_CODE = 69;
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_DENIED) {
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+
+
+
+
         wifiSignal = new WifiSignalDataCollector(getApplicationContext());
 
         signalButton = findViewById(R.id.signalButton);
+        scanButton = findViewById(R.id.scanButton);
+
         dataTextView = findViewById(R.id.dataTextView);
+        dataTextView.setMovementMethod(new ScrollingMovementMethod());
+        dataTextView.setText("");
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiSignal.scan();
+            }
+        });
 
         signalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               int level =  wifiSignal.calculateSignalLevel(5);
-               dataTextView.append("" + level + "\n");
+                List<ScanResult> result = wifiSignal.calculateSignalLevel(4);
+                if(result.size() > 0) {
+                    for(ScanResult r : result){
+                        if(r.SSID.equals("Westh")){
+                            dataTextView.append("" + r.level + "\n");
+                        }
+                    }
+
+               }
             }
         });
     }
--- app/src/main/java/com/example/pervasivepositioning/WifiSignalDataCollector.java
+++ app/src/main/java/com/example/pervasivepositioning/WifiSignalDataCollector.java
@@ -2,23 +2,43 @@ package com.example.pervasivepositioning;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WifiSignalDataCollector {
 
     int signalLevel;
     WifiManager wifiManager;
     WifiInfo wifiInfo;
+    Context context;
 
     @SuppressLint("ServiceCast")
     public WifiSignalDataCollector(Context context){
         this.wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         this.wifiInfo = wifiManager.getConnectionInfo();
+        this.context = context;
 
     }
 
-    public int calculateSignalLevel(int levels) {
-        return signalLevel = wifiManager.calculateSignalLevel(wifiInfo.getRssi(), levels);
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public List<ScanResult> calculateSignalLevel(int levels) {
+        wifiManager.startScan();
+        List<ScanResult> result = wifiManager.getScanResults();
+        for(ScanResult r : result){
+            Log.d("Scan", "ScanResult level: " + r.SSID + " "+ r.level);
+        }
+        return result;
+    }
+
+    public void scan(){
+        ((WifiManager) context.getSystemService(Context.WIFI_SERVICE)).startScan();
     }
 }
